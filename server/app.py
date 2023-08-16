ae69aea0c7199a76dd16aca074bb2bdee74420c5
bgn-dev <81875423+bgn-dev@users.noreply.github.com>
2023-08-16 22:42:34 +0200

    make it better

--- server/app.py
+++ server/app.py
@@ -4,7 +4,7 @@ import os, secrets, string
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate(os.getcwd() + "/key.json")
+cred = credentials.Certificate(os.getcwd() + "/server/key.json")
 #cred = credentials.Certificate('key.json') # for deployment
 firebase_admin.initialize_app(cred)
 db = firestore.client()
