2a20dabd36d824ccc4b5a126f9fadd4db0b7176e
bgn-dev <81875423+bgn-dev@users.noreply.github.com>
2023-08-21 23:38:13 +0200

    added navbar

---
+++ client/src/components/navbar.jsx
@@ -0,0 +1,41 @@
+import React, {useState} from 'react'
+import './navbar.css'
+
+import { useNavigate } from "react-router-dom";
+
+import { PiSignOutBold } from 'react-icons/pi';
+import { BiSearchAlt2 } from 'react-icons/bi';
+import { BiGroup } from 'react-icons/bi';
+
+
+
+export default function Navbar({ collabToggle, setCollabToggle }) {
+    const navigate = useNavigate();
+    const [clickedOnce, setClickedOnce] = useState(false);
+
+    function handleSignOut() {
+        localStorage.removeItem("currentUser");
+        navigate("/");
+    }
+
+    function collabsToggle() {
+        setCollabToggle(!collabToggle);
+        setClickedOnce(!clickedOnce);
+    }
+
+    return (
+        <div className="navbar-container">
+            <div className="navbar">
+                <div className="navbar-logo-container">
+                    <h1 className="navbar-logo">WebNote
+                        <p>Neat-Easy-Collaborative</p>
+                    </h1>
+                </div>
+                <input type="token" placeholder="Search" />
+                <button className="search"> <BiSearchAlt2 /> </button>
+                <button className={`toggle-collab ${clickedOnce ? 'button-clicked' : ''}`} onClick={() => collabsToggle()}> <BiGroup /> </button>
+                <button className="sign-out" onClick={() => handleSignOut()}> <PiSignOutBold /> </button>
+            </div>
+        </div>
+    )
+}
