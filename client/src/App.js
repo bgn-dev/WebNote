7f402d5a43d9e540d348473cdbe5e142ea5f235f
bgn-dev <81875423+bgn-dev@users.noreply.github.com>
2023-08-22 23:42:54 +0200

    adjustments and fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import './App.css';
 import Login from './components/login'
 import Grid from './components/grid'
 import Note from './components/note'
 import Collab from './components/collab'
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
