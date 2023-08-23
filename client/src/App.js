2f489677423feeaa3af41b77499a773a3d6539f9
bgn-dev <81875423+bgn-dev@users.noreply.github.com>
2023-08-23 21:23:13 +0200

    adjustments & added react-toast

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { BrowserRouter as BrowserRouter, Route, Routes } from "react-router-dom"
 import './App.css';
 import Login from './components/login'
 import Grid from './components/grid'
 import Note from './components/note'
 import Collab from './components/collab'
+import Popup from './components/popup'
 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -15,12 +16,28 @@ function App() {
   return (
     <div className="App">
       <div className="App-Header">
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/grid" element={<Grid />} />
-          <Route path="/note" element={<Note />} />
-          <Route path="/collab" element={<Collab />} />
-        </Routes>
+        <BrowserRouter>
+          <ToastContainer
+            position="top-right"
+            autoClose={20000}
+            limit={1}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick={false}
+            rtl={false}
+            pauseOnFocusLoss
+            draggable={false}
+            pauseOnHover
+            theme="light"
+          />
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/popup" element={<Popup />} />
+            <Route path="/grid" element={<Grid />} />
+            <Route path="/note" element={<Note />} />
+            <Route path="/collab" element={<Collab />} />
+          </Routes>
+        </BrowserRouter>
       </div>
     </div>
   );
