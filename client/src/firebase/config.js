e9ec99cb29a9aa539a39eb0340e8aa423ff05e43
bgn-dev <81875423+bgn-dev@users.noreply.github.com>
2025-08-17 13:41:00 +0200

    updated config.js to use environment variables

--- client/src/firebase/config.js
+++ client/src/firebase/config.js
@@ -4,16 +4,15 @@ import { getFirestore } from "@firebase/firestore";
 import { getAuth } from "firebase/auth"
 import { GoogleAuthProvider } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC1A8BoaSfkbuRoJPyJhmrnSuduLft3WzY",
-  authDomain: "webnote-df968.firebaseapp.com",
-  projectId: "webnote-df968",
-  storageBucket: "webnote-df968.appspot.com",
-  messagingSenderId: "785362641992",
-  appId: "1:785362641992:web:e5369bbaf2844d897c647d",
-  measurementId: "G-DJCXVFVL9K"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
